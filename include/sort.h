d0f86e033436f83702382d5177b1ed9cbcbee82a
IAmBornedHope <realbeloff@gmail.com>
2025-12-09 20:19:39 +0300

    Added bubble_sort function

---
+++ include/sort.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "compare.h"
+#include "article.h"
+#include "list.h"
+
+/**
+ * @brief Пузырьковая сортировка двусвязного списка с использованием функции-компаратора
+ * 
+ * @param list 
+ * @param compare_articles 
+ */
+void bubble_sort(List* list, int (*compare_articles)(const Article*, const Article*));
